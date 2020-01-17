508728b2fec784f6d4b5cef6150b1f636ed32523
Mirt8 <0508237@mlsd.org>
2020-01-16 16:14:36 -0800

    Filled
    Typo in command name

--- src/main/java/robot/commands/shooterFireContinousCmd.java
+++ src/main/java/robot/commands/shooterFireContinousCmd.java
@@ -43,6 +43,7 @@ public class shooterFireContinousCmd extends Command {
     // Called repeatedly when this Command is scheduled to run
     @Override
     protected void execute() {
+        Robot.shooterSubSys.moveFlywheel(1);
     }
 
     // Make this return true when this Command no longer needs to run execute()
